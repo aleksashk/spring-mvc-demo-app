50b814df6078cd8a26a12e9a97a5571a4cc2d04c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-27 17:04:31 +0300

    java configuration

--- src/main/java/by/flameksandr/spring/mvc/HelloController.java
+++ src/main/java/by/flameksandr/spring/mvc/HelloController.java
@@ -1,8 +1,13 @@
 package by.flameksandr.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
 }
