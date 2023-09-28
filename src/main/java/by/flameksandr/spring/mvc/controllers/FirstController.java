28b9a8b9e87c897f149f07bfb432cb28f0a731e3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-28 20:06:39 +0300

    @RequestParam with thymeleaf

--- src/main/java/by/flameksandr/spring/mvc/controllers/FirstController.java
+++ src/main/java/by/flameksandr/spring/mvc/controllers/FirstController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,7 +20,8 @@ public class FirstController {
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(@RequestParam("surname") String surname, Model model) {
+        model.addAttribute("lastName", surname);
         return "first/goodbye";
     }
 }
